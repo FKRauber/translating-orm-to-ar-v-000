bbf1d03a3ac04341ede45277144ae5e6c8a45d4d
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-07-19 01:13:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,32 +2,6 @@ class Dog < ActiveRecord::Base
 
   attr_accessor :name, :breed
 
-  def self.create(name:, breed:)
-    dog = Dog.new(name, breed)
-    dog.save
-    dog
-  end
 
-  def self.save
-    dog = Dog.find_by_name(name)
-    dog.save
-    dog
-  end
-
-  def self.update
-
-  end
-
-  def self.find_or_create_by
-    dog = Dog.
-  end
-
-  def self.find_by_name(name)
-
-  end
-
-  def self.find_by_id
-
-  end
 
 end
