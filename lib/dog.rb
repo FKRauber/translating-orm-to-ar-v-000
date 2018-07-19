c984cfe7dbb97b62cec960b084ecd163151cb235
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-07-19 01:05:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,17 +3,13 @@ class Dog < ActiveRecord::Base
   attr_accessor :name, :breed
 
   def self.create(name:, breed:)
-    sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS dogs (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        breed TEXT
-      )
-    SQL
+    dog = Dog.new(name, breed)
+    dog.save
+    dog
   end
 
   def self.save
-    
+
   end
 
 end
