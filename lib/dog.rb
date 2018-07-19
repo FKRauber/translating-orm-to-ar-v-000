c0aaf875599815ffd2924b5f24a1601ec64c98d5
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-07-19 01:14:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,17 @@ class Dog < ActiveRecord::Base
 
   attr_accessor :name, :breed
 
+  def self.update
+
+  end
+
+  def self.find_or_create_by
+    dog = Dog.
+  end
+
+  def self.find_by_name(name)
+
+  end
 
 
 end
